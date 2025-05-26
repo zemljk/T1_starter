e40e79d7fefc8dccdcd6d9ff3fc0a5541b4b8165
zemljk <136977502+zemljk@users.noreply.github.com>
2025-05-26 09:52:51 +0400

    my_starter++

---
+++ src/main/java/ru/t1/mystarterlogs/aspect/annotaion/TrackExecutionTime.java
@@ -0,0 +1,12 @@
+package ru.t1.mystarterlogs.aspect.annotaion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE_USE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TrackExecutionTime {
+
+}
